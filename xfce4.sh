c42481c2f4cdb32bae11b57f9f2350dcf328bad2
Phạm Duy <58114272+phamduy091@users.noreply.github.com>
2021-10-26 21:46:15 +0700

    up

--- xfce4.sh
+++ xfce4.sh
@@ -1,5 +1,5 @@
 echo "Download ngrok"
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip > /dev/null 2>&1
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
@@ -8,16 +8,25 @@ nohup ./ngrok tcp 3389 &>/dev/null &
 echo Downloading File From akuh.net
 echo "===================================="
 echo "Wait 2 Minutes"
+echo "This is only for education"
+echo "Other interesting tutorials visit akuh.net"
+echo "===================================="
+sudo apt-get update > /dev/null 2>&1
+echo "===================================="
+echo "Install Firefox"
+echo "===================================="
+sudo apt install firefox -y > /dev/null 2>&1
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
-Start-Process Powershell -ArgumentList '-Noexit -Command ".\ngrok\ngrok.exe tcp 3389"'
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo apt install xfce4 -y > /dev/null 2>&1
+sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
 echo "===================================="
 echo "Start RDP"
 echo "===================================="
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
