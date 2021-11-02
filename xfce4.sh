96e50a78b7220ad5f713052ab0dba26e99058246
Phạm Duy <58114272+phamduy091@users.noreply.github.com>
2021-11-03 00:09:39 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -2,7 +2,7 @@ echo "Download ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
-import CRP
+read -p "Ctrl + V Authtoken: " CRP #@param {type:'string'}
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region us 3389 &>/dev/null &
 echo Downloading File From akuh.net
